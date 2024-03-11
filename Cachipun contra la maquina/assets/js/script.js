3d55ccc2e4218f80b34748863bb576570613aacb
Juan Alvarez <juan.alvarez1989@gmail.com>
2024-03-11 18:13:08 -0300

    Script terminado

--- Cachipun contra la maquina/assets/js/script.js
+++ Cachipun contra la maquina/assets/js/script.js
@@ -1,40 +1,35 @@
-/*Counter maquina - humano -->
-<div>
-    Contador
-    Maquina - humano
-</div>
-<!-- elegir jugada: rock, paper, scissors-->
-<!-- resultados cada partida-->
-<!-- generar una jugada automatica de la maquina-->
-<!--definir un ganador
-felicitar al ganador en caso de ser el usuario
-Indicarle al usuario que ha perdido contra la máquina en caso de que ésta sea
-la que gane.
-● Declarar un empate.  */
-
+//Se asegura que el código se ejecute despues de que el DOM haya sido completamente cargado
 $(document).ready(function () {
+    //contadores
     var counterUser = 0;
     var counterMachine = 0;
+    //solicita al usuario las veces que desea que se repita el juego
     var round = prompt("How many rounds do you want to play? (1-5)");
     var userOption;
     var machineOption;
-
     const userChoice = {
         1: "rock",
         2: "paper",
         3: "scissors"
     };
+
+    /* condicional para que el número de jugadas esté entre el rango 1 -5.
+    El número de jugadas debe ser indicado por el usuario, por temas prácticos de prueba se redujo a 5. 
+    Se podría modificar y dejar solo: "if (round > 0){..." para asegurar que el valor ingresado sea mayor a 0 */
     if (round > 0 && round < 6) {
         alert("Let's start");
         var results = [];
-        for (i = 0; i < round; i++) {
+        for (i = 0; i < round; i++) { 
+            //solicita una jugada al usuario en cada ronda
             userOption = prompt("Choose an option:\n 1. Rock\n 2. Paper\n 3. Scissors");
-            while (userOption < 1 || userOption > 3) {
+            while (userOption < 1 || userOption > 3) { //se repite hasta obtener una respuesta en el rango correcto
                 alert('Invalid option. Please choose between 1 -3.');
                 userOption = prompt("Choose an option:\n 1. Rock\n 2. Paper\n 3. Scissors");
             }
             userOption = userChoice[userOption];
-            machineOption = userChoice[Math.floor((Math.random() * 3) + 1)];
+            machineOption = userChoice[Math.floor((Math.random() * 3) + 1)]; //genera una jugada automática para la maquina usando Math.random
+            /*if, else if y else están dentro de un for, se repetirán la cantidad de veces que el usuario indicó.
+            Cada uno de ellos tiene un alert que muestra el resultado de cada ronda inmediatamente */
             if (userOption === machineOption) {
                 results.push(`It's a tie: Your choice: ${userOption} vs. Machine's choice: ${machineOption}`);
                 alert(results[i]);   
@@ -45,18 +40,17 @@ $(document).ready(function () {
                 
             ) {
                 results.push(`You win: Your choice: ${userOption} vs. Machine's choice: ${machineOption}`);
-                counterUser++;
+                counterUser++; //suma 1 punto al contador del usuario cuando gana
                 alert(results[i]);   
             } else {
                 results.push(`You lose: Your choice:  ${userOption} vs. Machine's choice: ${machineOption}`);
-                counterMachine++;
+                counterMachine++; //suma 1 punto al contador de la maquina cuando gana
                 alert(results[i]);   
             }          
         }
-    /*   for (i = 0; i < results.length; i++) {
-            alert(results[i]);
-        } */
+        //alert muestra los resultados finales de cada contador
         alert(`Final results:\nYou: ${counterUser} \nMachine: ${counterMachine}`);
+        //condicional, indica el resultado de la partida dependiendo del caso
         if (counterUser > counterMachine) {
             alert("Congratulations! You won the game!");
         }
