832eb203994b6d290ab7b4a29bdcb55b46b063fd
Juan Alvarez <juan.alvarez1989@gmail.com>
2024-03-13 01:45:07 -0300

    desafio multiplicacion

---
+++ Desafio Multiplicacion/assets/js/script.js
@@ -0,0 +1,30 @@
+/*$(document).ready(function () {
+});
+*/
+
+//Función arrow para calcular el factorial de un número. Está almacenada dentro de una const
+const calculoFactorial = (numero) => {
+    if (numero < 0) {
+        return "No se puede calcular el factorial de un número negativo.";
+    }
+    let factorial = 1;
+    for (let i = 2; i <= numero; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
+
+//Función arrow: valida que el número esté entre 1 y 20. Realiza las multiplicaciones y factorial hasta el número indicado
+const validarNumero = (numeroValidado) => {
+    if (num < 1 || num > 20) { alert("Número fuera de rango") } else {
+        for (let i = 1; i <= num; i++) { //ciclo for. Ejecuta la multiplicacion
+            console.log(`${i} x ${num} = ${i * num}`);
+        }
+        for (let i = 1; i <= num; i++) { //ciclo que calcula el factorial de i hasta el número seleccionado
+            console.log(`Factorial de ${i} es: ${(calculoFactorial(i))}`); //${(calculoFactorial)} llamado a la función
+        }
+    }
+}
+
+(validarNumero(num=prompt("Ingresa un número entre 1 a 20"))); //Llamado a la función
+
